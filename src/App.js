511c15be046b5d698d71e77b9d03934dae6dfd2f
Mohan Kannan <mohan11061999@outlook.com>
2023-03-02 10:58:25 +0530

    map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const categories = [
+  {
+    id : 1,
+    title:"Hate",
+  },
+  {
+    id : 2,
+    title:"Jackets",
+  },
+  {
+    id : 3,
+    title:"Sneakers",
+  },
+  {
+    id : 4,
+    title:"Women",
+  },
+  {
+    id : 5,
+    title:"Men",
+  }
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="categorys-container">
+      {categories.map(({title})=>(
+        <div className="category-container">
+        {/*<img />*/}
+        <div className="category-body-container">
+        <h1>{title}</h1>
+        <p>Shop Now</p>
+        </div>
+      </div>
+      ))}
+    </div>   
   );
-}
+};
 
 export default App;
