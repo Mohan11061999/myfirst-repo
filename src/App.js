21e20d8f4305d2a85047a19ce3ffbee2469c3724
Mohan Kannan <mohan11061999@outlook.com>
2023-03-03 19:02:21 +0530

    directory component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import './categories.styles.scss'
-import CategoryItem from "./components/category-item/category-item";
 
+import Directory from "./components/directory/directory.component";
 
 const categories = [
   {
@@ -34,11 +33,7 @@ const categories = [
 
 const App = () => {
   return (
-    <div className="categories-container">
-      {categories.map((category)=>(
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>   
+       <Directory categories={categories} />
   );
 };
 
